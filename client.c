c08411ec91fd1070c6b4a450b132c00bfcddcf4f
Gintoki54 <luca.cordella@telecomnancy.net>
2018-07-01 16:45:18 +0200

    ajout de l'authentification avec SHA256

--- client.c
+++ client.c
@@ -7,13 +7,19 @@
 #include <sys/socket.h>
 #include <string.h>
 #include <openssl/aes.h>
+#include <assert.h>
+#include <unistd.h>
+#include <arpa/inet.h>
 #define SERVEURNAME "127.0.0.1" // adresse IP de mon serveur
-
+#define PORTNUMBER 6554
 int to_server_socket = -1;
 int errflg = 0;
 int fileflag=0;
 int nameflag=0;
-      
+int passflag=0;
+int createflag=0;
+char *file_name;
+
 void hexdump(FILE *f, const char *title, const unsigned char *s, int length)
 {
     for(int n = 0; n < length ; ++n) {
@@ -23,6 +29,54 @@ void hexdump(FILE *f, const char *title, const unsigned char *s, int length)
     }
     fprintf(f, "\n");
 }    
+
+char** str_split(char* a_str, const char a_delim)
+{
+    char** result    = 0;
+    size_t count     = 0;
+    char* tmp        = a_str;
+    char* last_comma = 0;
+    char delim[2];
+    delim[0] = a_delim;
+    delim[1] = 0;
+
+    /* Count how many elements will be extracted. */
+    while (*tmp)
+    {
+        if (a_delim == *tmp)
+        {
+            count++;
+            last_comma = tmp;
+        }
+        tmp++;
+    }
+
+    /* Add space for trailing token. */
+    count += last_comma < (a_str + strlen(a_str) - 1);
+
+    /* Add space for terminating null string so caller
+       knows where the list of returned strings ends. */
+    count++;
+
+    result = malloc(sizeof(char*) * count);
+
+    if (result)
+    {
+        size_t idx  = 0;
+        char* token = strtok(a_str, delim);
+
+        while (token)
+        {
+            assert(idx < count);
+            *(result + idx++) = strdup(token);
+            token = strtok(0, delim);
+        }
+        assert(idx == count - 1);
+        *(result + idx) = 0;
+    }
+
+    return result;
+}
 
 
 void main (int argc, char *argv[]){
@@ -35,25 +89,65 @@ void main (int argc, char *argv[]){
 	char buffer[512];
 	int c,sz;
 	char size_array[12]; 
+	char createflag_array[12];
+	char fileflag_array[12];
 	extern char * optarg; 
 	extern int optind, opterr; 
 	char *progname = argv[0];
 	FILE *fp;
 	FILE *fkey;
 	char chaine[1024];
-	char *file_name;
 	char *user_name;
 	char *tot_chaine;
-	  while ((c = getopt(argc , argv, "f:n:")) != -1)
+	char **tokens;
+	int size_file_name=0;
+	char *final_file_name = (char *) malloc(500);
+	char *file_name_tmp = (char *) malloc(500);
+	char *key;
+	char *password;
+	char newlogin[50];
+	char newpass[50];
+
+	  while ((c = getopt(argc , argv, "f:n:p:c")) != -1)
 	    switch (c) {
 	    case 'f':
 	      file_name = optarg;
+	      strcpy(file_name_tmp,file_name);
+	      tokens = str_split(file_name,'/');
+	      printf("%s\n",file_name_tmp);
+	      while(*(tokens+size_file_name)){
+		size_file_name++;
+	      }
+	      strcpy(final_file_name, *(tokens+size_file_name-1));
+	      fp = fopen(file_name_tmp,"r");
+	      if(fp){
+	      	fseek(fp, 0L, SEEK_END);
+              	sz = ftell(fp);
+              	fseek(fp, 0L, SEEK_SET);
+	      }else{
+		printf("Impossible d'ouvrir le fichier\n");
+		exit(1);
+	      }
 	      fileflag++;
 	      break;
 	    case 'n':
 	      user_name=optarg;
 	      nameflag++;
 	      break;
+	    case 'p':
+              password=optarg;
+              passflag++;
+              break;
+	    case 'c':
+              createflag++;
+	      fileflag=0;
+	      passflag=0;
+	      nameflag=0;
+	      printf("Entrez le login de votre compte:\n");
+	      scanf("%s",newlogin);
+	      printf("Entrez le mot de passe de votre compte:\n");
+    	      scanf("%s",newpass);
+              break;	    
 	    case '?':
 	      errflg++;
 	      break;
@@ -62,18 +156,22 @@ void main (int argc, char *argv[]){
 	    fprintf(stderr, "usage: %s [-n arg] [-f arg]\n", progname);
 	    exit(1);
         }
-	if(fileflag){    
-	    printf("%s\n", file_name);
-	    fp = fopen(file_name,"r");
+	if(createflag ==0){
+		if(nameflag && passflag){
+            		printf("Hello\t%s\n", user_name);
+		}else{
+	    		fprintf(stderr,"Login ou mot de passe non renseignés !!");
+	    		exit(1);
+        	}
+	}
+	unsigned char enc_out[sz];
+	if(fileflag){  
 	    if(!fp){
                fprintf(stderr, "Le fichier n'existe pas\n");
 	       exit(1);
 	    }
 	    else{
  		printf("Fichier OK !!\n");
-		fseek(fp, 0L, SEEK_END);
-		sz = ftell(fp);
-		fseek(fp, 0L, SEEK_SET);
 		tot_chaine = (char *) malloc(sz*sizeof(char));
 		while(fgets(chaine, 1024, fp)!=NULL){ 
 			strcat(tot_chaine,chaine);
@@ -81,9 +179,9 @@ void main (int argc, char *argv[]){
     		fclose(fp);
 		system("openssl enc -aes-128-cbc -k secret -P -md sha1 >> key.dat");
 		fkey = fopen("key.dat","r");
-		char *key;
 		char *iv;
 		if(fkey){
+			printf("Traitement de la clé\n");
 			while(fgets(chaine, 1024, fkey)!=NULL){
 			
                         	if(chaine[0]=='k'){
@@ -95,8 +193,9 @@ void main (int argc, char *argv[]){
 			fclose(fkey);
 			system("rm key.dat");
 			//const unsigned char *encode_key= (const unsigned char *) strtol(key,NULL,16);
-			//const unsigned char *encode_content= (const unsigned char *) strtol(tot_chaine,NULL,16);
-		        unsigned char enc_out[sz];
+			//const unsigned char *encode_content= (const unsigned char *) strtol(tot_chaine,NULL,16);	
+			
+			//unsigned char enc_out_tmp[sz];
 			unsigned char dec_out[sz];
 		        unsigned char iv2[AES_BLOCK_SIZE];
 			memset(iv2,0x00,AES_BLOCK_SIZE);
@@ -104,19 +203,19 @@ void main (int argc, char *argv[]){
         		AES_set_encrypt_key((unsigned char*) key, 128, &enc_key);
 			//hexdump(stdout, "AES_KEY", (unsigned char*) &enc_key, sizeof(AES_KEY));
 			AES_cbc_encrypt((unsigned char*) tot_chaine, enc_out, sz,&enc_key, iv2, AES_ENCRYPT);
+			
 			//hexdump(stdout, "AES_ENCRYPT", (unsigned char*) &enc_out, sizeof(enc_out));
 			
-			memset(iv2,0x00,AES_BLOCK_SIZE);
+			/*memset(iv2,0x00,AES_BLOCK_SIZE);
         		AES_set_decrypt_key((unsigned char*) key, 128, &dec_key);
 			AES_cbc_encrypt(enc_out, dec_out, sz,&dec_key, iv2, AES_DECRYPT);
 			//hexdump(stdout, "AES_DECRYPT", (unsigned char*) &dec_out, sizeof(dec_out));
+			char *chaine_dechiffree =(char *) dec_out;
+			printf("%d\n",sz);*/
 		        	
 		}
 	    } 	
 	}
-	if(nameflag)
-            printf("Hello\t%s\n", user_name);
-
 	bzero(&serverSockAddr,sizeof(serverSockAddr));
 	hostAddr = inet_addr(SERVEURNAME);
 	if ( (long)hostAddr != (long)-1)
@@ -131,7 +230,7 @@ void main (int argc, char *argv[]){
 		}
 		bcopy(serverHostEnt->h_addr,&serverSockAddr.sin_addr,serverHostEnt->h_length);
 	}
-	serverSockAddr.sin_port = htons(3672);
+	serverSockAddr.sin_port = htons(PORTNUMBER);
 	serverSockAddr.sin_family = AF_INET;
 
 	if ( (to_server_socket = socket(AF_INET,SOCK_STREAM,0)) < 0)
@@ -145,17 +244,147 @@ void main (int argc, char *argv[]){
 		printf("demande de connection ratee\n");
 		exit(1);
 	}
-        if(fileflag>0){
-		//printf("%s\n",tot_chaine);
-		write(to_server_socket,file_name,strlen(file_name));
-		read(to_server_socket,buffer,512);
+	
+	int n;	
+		if((n = recv(to_server_socket,buffer,sizeof(buffer),0))==-1){
+                	fprintf(stderr, "Failure Receving Message\n");
+            		close(to_server_socket);
+            		exit(1);
+		}
+                printf("%s\n",buffer);
+		sprintf(fileflag_array,"%d",fileflag);
+		if((n=send(to_server_socket,fileflag_array,sizeof(fileflag_array),0))==-1){
+			fprintf(stderr, "Failure Sending create flag\n");
+                        close(to_server_socket);
+                        exit(1);
+		}
+	
+		if((n=recv(to_server_socket,buffer,sizeof(buffer),0))==-1){
+                        fprintf(stderr, "Failure Receving message conf create flag\n");
+                        close(to_server_socket);
+                        exit(1);
+                }	
+                printf("%s\n",buffer);
+		sprintf(createflag_array,"%d",createflag);
+		if((n=send(to_server_socket,createflag_array,sizeof(createflag_array),0))==-1){
+                        fprintf(stderr, "Failure Sending create flag\n");
+                        close(to_server_socket);
+                        exit(1);
+                }
+
+                if((n=recv(to_server_socket,buffer,sizeof(buffer),0))==-1){
+                        fprintf(stderr, "Failure Receving message conf create flag\n");
+                        close(to_server_socket);
+                        exit(1);
+                }
+                printf("%s\n",buffer);
+	if(createflag>0){	
+		printf("Création de votre compte en cours %s\n",newlogin);
+		if((n=send(to_server_socket,newlogin, strlen(newlogin),0))==-1){
+                        fprintf(stderr, "Failure Sending Login\n");
+                        close(to_server_socket);
+                        exit(1);
+                }
+                if((n = recv(to_server_socket,buffer,512,0))==-1){
+                        fprintf(stderr, "Failure Receving Message\n");
+                        close(to_server_socket);
+                        exit(1);
+                }
+                printf("%s\n",buffer);
+		if((n=send(to_server_socket,newpass, strlen(newpass),0))==-1){
+                        fprintf(stderr, "Failure Sending password\n");
+                        close(to_server_socket);
+                        exit(1);
+                }
+                if((n = recv(to_server_socket,buffer,512,0))==-1){
+                        fprintf(stderr, "Failure Receving Message\n");
+                        close(to_server_socket);
+                        exit(1);
+                }
+                printf("%s\n",buffer);
+
+		if((n = recv(to_server_socket,buffer,512,0))==-1){
+                        fprintf(stderr, "Failure Receving Message\n");
+                        close(to_server_socket);
+                        exit(1);
+                }
+                printf("%s\n",buffer);
+	}
+
+	
+	if(fileflag>0){
+
+	        if((n=send(to_server_socket,user_name, strlen(user_name),0))==-1){
+                        fprintf(stderr, "Failure Sending usernamee\n");
+                        close(to_server_socket);
+                        exit(1);
+                }
+                if((n = recv(to_server_socket,buffer,512,0))==-1){
+                        fprintf(stderr, "Failure Receving Message\n");
+                        close(to_server_socket);
+                        exit(1);
+                }
+                printf("%s\n",buffer);
+
+		if((n=send(to_server_socket,password, strlen(password),0))==-1){
+                        fprintf(stderr, "Failure Sending password\n");
+                        close(to_server_socket);
+                        exit(1);
+                }
+                if((n = recv(to_server_socket,buffer,512,0))==-1){
+                        fprintf(stderr, "Failure Receving Message\n");
+                        close(to_server_socket);
+                        exit(1);
+                }
+                printf("%s\n",buffer);	
+		
+		printf("Enregistrement du fichier %s\n",final_file_name);
+		if((n=send(to_server_socket,key, strlen(key)-1,0))==-1){
+			fprintf(stderr, "Failure Sending Key\n");
+            		close(to_server_socket);
+            		exit(1);
+		}
+		if((n = recv(to_server_socket,buffer,512,0))==-1){
+			fprintf(stderr, "Failure Receving Message\n");
+            		close(to_server_socket);
+            		exit(1);
+		}
 		printf("%s\n",buffer);
+		if((n=send(to_server_socket,final_file_name,strlen(final_file_name)-1,0))==-1){
+			fprintf(stderr, "Failure Sending File Name\n");
+                        close(to_server_socket);
+                        exit(1);
+		}
+		
+		if((n=recv(to_server_socket,buffer,512,0))==-1){
+			fprintf(stderr, "Failure Receving Confirmation\n");
+                        close(to_server_socket);
+                        exit(1);
+		}
+		printf("%s\n",buffer);
+
 		sprintf(size_array,"%d",sz);
-		write(to_server_socket,size_array,strlen(size_array));
-		read(to_server_socket,buffer,512);
-		printf("%s\n",buffer);	
-		write(to_server_socket,tot_chaine,sz);
-		read(to_server_socket,buffer,512);
+		if((n=send(to_server_socket,size_array,sizeof(size_array),0))==-1){
+		 	fprintf(stderr, "Failure Sending size file\n");
+                        close(to_server_socket);
+                        exit(1);	
+		}
+		if((n=recv(to_server_socket,buffer,512,0))==-1){
+			fprintf(stderr, "Failure Receving Confirmation size\n");
+                        close(to_server_socket);
+                        exit(1);
+		}
+		printf("%s\n",buffer);
+		if((n=send(to_server_socket,enc_out,sz,0))==-1){
+			fprintf(stderr, "Failure Sending Cipher text\n");
+                        close(to_server_socket);
+                        exit(1);
+		}
+		if((n=recv(to_server_socket,buffer,512,0))==-1){
+			fprintf(stderr, "Failure Receving Confirmation cipher text\n");
+                        close(to_server_socket);
+                        exit(1);
+		}
 		printf("%s\n",buffer);	
 
         }
--- server.c
+++ server.c
@@ -5,19 +5,37 @@
 #include <netinet/in.h>
 #include <signal.h>
 #include <string.h>
-
+#include <unistd.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <errno.h>
+#include <openssl/aes.h>
+#include <openssl/sha.h>
+#include <openssl/evp.h>
+#define PORTNUMBER 6554
 void fin (int i);
-char buffer[4096];
+char buffer[512];
 int ma_socket;
+FILE *accounts;
+void hexdump(FILE *f, const char *title, const unsigned char *s, int length)
+{
+    for(int n = 0; n < length ; ++n) {
+        if((n%16) == 0)
+            fprintf(f, "\n%s  %04x", title, n);
+        fprintf(f, " %02x", s[n]);
+    }
+    fprintf(f, "\n");
+}
+
 
 void main (int argc, char *argv[]){
 	int client_socket;
 	struct sockaddr_in mon_address, client_address;
 	int mon_address_longueur, lg,size_file;
-	char file_name[500];
-	char content_file[5000];
+	char file_name[512];
+	//char content_file[5000];
 	bzero(&mon_address,sizeof(mon_address));
-	mon_address.sin_port = htons(3672);
+	mon_address.sin_port = htons(PORTNUMBER);
 	mon_address.sin_family = AF_INET;
 	mon_address.sin_addr.s_addr = htonl(INADDR_ANY);
 
@@ -34,33 +52,272 @@ void main (int argc, char *argv[]){
 	listen(ma_socket,5);
 	/* accept la connexion */
 	mon_address_longueur = sizeof(client_address);
+	char *key = (char *) malloc(32);
+	int createflag=0;
+	int fileflag=0;
+	char newlogin[50];
+	char newpass[50];
+	memset(newlogin,0,50);
+	memset(newpass,0,50);
 	while(1){
 		client_socket = accept(ma_socket,
 		(struct sockaddr *)&client_address,
 		&mon_address_longueur);
 		if (fork() == 0){
 			close(ma_socket);
+			if((lg=send(client_socket,"Connecté au serveur",111,0))==-1){
+				fprintf(stderr, "Failure Sending Message\n");
+                        	close(client_socket);
+                        	exit(1);
+			}
+			if((lg=recv(client_socket,buffer,512,0))==-1){
+                                fprintf(stderr, "Failure Receving File flag\n");
+                                close(client_socket);
+                                exit(1);
+                        }
+			fileflag = atoi(buffer);
+			printf("File Flag=%d\n",fileflag);
+			if((lg=send(client_socket,"File flag reçu",111,0))==-1){
+                                fprintf(stderr, "Failure Sending Message\n");
+                                close(client_socket);
+                                exit(1);
+                        }
+			if((lg=recv(client_socket,buffer,512,0))==-1){
+                                fprintf(stderr, "Failure Receving Create flag\n");
+                                close(client_socket);
+                                exit(1);
+                        }
+                        createflag = atoi(buffer);
+                        printf("Create Flag=%d\n",createflag);
+                        if((lg=send(client_socket,"Create flag reçu",111,0))==-1){
+                                fprintf(stderr, "Failure Sending Message\n");
+                                close(client_socket);
+                                exit(1);
+                        }
+			if(createflag>0){
+				fileflag=0;
+				if((lg=recv(client_socket,newlogin,512,0))==-1){
+                                	fprintf(stderr, "Failure Receving Login\n");
+                                	close(client_socket);
+                                	exit(1);
+                        	}
+				printf("%s\n",newlogin);
+				int is_present=0;
+				char cmd_dir[50] = "mkdir ./Server/";
+                                strcat(cmd_dir,newlogin);
+                                is_present = system(cmd_dir);
+				printf("%d\n",is_present);
+				if(is_present == 256){
+                                	if((lg=send(client_socket,"Login déjà pris",51,0))==-1){
+                                        	fprintf(stderr, "Failure Sending Confirmation\n");
+                                        	close(client_socket);
+                                        	exit(1);
+                                	}
+				}
+				else{
+					if((lg=send(client_socket,"new login reçu ",111,0))==-1){
+						fprintf(stderr, "Failure Sending Message\n");
+						close(client_socket);
+						exit(1);
+					}
 
-			lg = read(client_socket,buffer, 512);
-			strcpy(file_name,buffer);
-			//sprintf(buffer,"%s du serveur",buffer);
-			write(client_socket,"Nom du fichier reçu",512);
-			lg=read(client_socket,buffer,512);
-				//strcat(content_file,buffer);
-			size_file = atoi(buffer);
-		        printf("%d\n",size_file);	
-			write(client_socket,"Taille du fichier reçu !!!",512);	
-			lg=read(client_socket,buffer,size_file);
-		        strcpy(content_file,buffer);
-			printf("%s\n",content_file);
-			write(client_socket,"Contenu du fichier reçu !!!",512);	
+					
+					if((lg=recv(client_socket,newpass,512,0))==-1){
+						fprintf(stderr, "Failure Receving Password\n");
+						close(client_socket);
+						exit(1);
+					}
+					printf("%s\n",newpass);
+
+					if((lg=send(client_socket,"new password reçu ",111,0))==-1){
+						fprintf(stderr, "Failure Sending Message\n");
+						close(client_socket);
+						exit(1);
+					}
+					 EVP_MD_CTX *mdctx;
+                                	mdctx = EVP_MD_CTX_create();
+                                	unsigned char digest[EVP_MAX_MD_SIZE];
+                                	unsigned int digest_len;
+                                	int i;
+                                	EVP_DigestInit(mdctx, EVP_sha256());
+                                	EVP_DigestUpdate(mdctx, newpass, strlen(newpass));
+                                	EVP_DigestFinal(mdctx, digest, &digest_len);
+                                	EVP_MD_CTX_destroy(mdctx);
+                                	char hash[digest_len];
+                                	memset(hash,0,digest_len);
+                                	char hash_tmp[2];
+                                	for(i=0;i<digest_len;i++){
+                                        	sprintf(hash_tmp,"%02x",digest[i]);
+                                        	strcat(hash,hash_tmp);
+                                	}
+
+                                	printf("%s\n",hash);
+
+					accounts = fopen("accounts.txt","a");
+					char account[512];
+					strcpy(account,newlogin);
+					strcat(account,":");
+					strcat(account,hash);
+					fputs(account,accounts);
+					fclose(accounts);
+					if((lg=send(client_socket,"Compte crée",51,0))==-1){
+						fprintf(stderr, "Failure Sending Confirmation\n");
+						close(client_socket);
+						exit(1);
+					}
+				}
+			}
+			if(fileflag>0){
+				char current_username[50];
+				char current_password[50];
+				memset(current_username,0,50);
+				memset(current_password,0,50);
+				if((lg=recv(client_socket,current_username,512,0))==-1){
+                                        fprintf(stderr, "Failure Receving user name\n");
+                                        close(client_socket);
+                                        exit(1);
+                                }
+				printf("%s\n",current_username);
+				if((lg=send(client_socket,"Username reçu",51,0))==-1){
+                                        fprintf(stderr, "Failure Sending Confirmation\n");
+                                        close(client_socket);
+                                        exit(1);
+                                }
+
+				if((lg=recv(client_socket,current_password,512,0))==-1){
+                                        fprintf(stderr, "Failure Receving password\n");
+                                        close(client_socket);
+                                        exit(1);
+                                }
+                                //printf("%s\n",current_password);
+                                if((lg=send(client_socket,"Password reçu",51,0))==-1){
+                                        fprintf(stderr, "Failure Sending Confirmation\n");
+                                        close(client_socket);
+                                        exit(1);
+                                }
+				EVP_MD_CTX *mdctx;
+				mdctx = EVP_MD_CTX_create();
+				unsigned char digest[EVP_MAX_MD_SIZE];
+				unsigned int digest_len;
+				int i;
+				EVP_DigestInit(mdctx, EVP_sha256());
+				EVP_DigestUpdate(mdctx, current_password, strlen(current_password));
+				EVP_DigestFinal(mdctx, digest, &digest_len);
+				EVP_MD_CTX_destroy(mdctx);
+				char hash[digest_len];
+				memset(hash,0,digest_len);
+				char hash_tmp[2];
+				for(i=0;i<digest_len;i++){
+					sprintf(hash_tmp,"%02x",digest[i]);
+					strcat(hash,hash_tmp);
+				}
+				hash[strlen(hash)]='\0';
+				
+				//printf("%s\n",hash);
+				FILE *accounts;
+				accounts = fopen("accounts.txt","r");
+				char account[512];
+				char *account_username;
+				char *account_hash;
+				int auth = 0;
+				int user_cmp;
+				int pass_cmp;
+				while(fgets(account, 512, accounts)!=NULL){
+					account_username = strtok_r(account,":",&account_hash);
+					account_hash[strlen(account_hash)-1] = '\0';
+					if(strcmp(account_username,current_username)==0){
+						if(strcmp(account_hash,hash)==0){
+							auth=1;
+							break;
+						}
+					}	
+                                	
+                        	}
+				fclose(accounts);
+				printf("Auth = %d\n",auth);
+				if(auth==0){
+					 if((lg=send(client_socket,"Mot de passe ou login incorrects",512,0))==-1){
+                                                fprintf(stderr, "Failure Sending Confirmation\n");
+                                                close(client_socket);
+                                                exit(1);
+                                        }
+					
+
+				}else{
+					
+					if((lg=recv(client_socket,buffer,512,0))==-1){
+						fprintf(stderr, "Failure Receving Key\n");
+						close(client_socket);
+						exit(1);
+					}
+					
+					strcpy(key,buffer);
+					//printf("%s\n",key);
+					memset(buffer,0,512);
+					if((lg=send(client_socket,"Key reçue",51,0))==-1){
+						fprintf(stderr, "Failure Sending Confirmation\n");
+						close(client_socket);
+						exit(1);
+					}
+					if((lg=recv(client_socket,file_name,512,0))==-1){
+						fprintf(stderr, "Failure Receving File name\n");
+						close(client_socket);
+						exit(1);
+					}
+					//strcpy(file_name,buffer);
+					//printf("%s\n",file_name);
+					memset(buffer,0,512);
+					if((lg=send(client_socket,"Nom du fichier reçu",100,0))==-1){
+						fprintf(stderr, "Failure Sending Confirmation file\n");
+						close(client_socket);
+						exit(1);
+					}
+					if((lg=recv(client_socket,buffer,512,0))==-1){
+						fprintf(stderr, "Failure Receving Size file\n");
+						close(client_socket);
+						exit(1);	
+					}
+					size_file = atoi(buffer);
+					printf("%d\n",size_file);
+					if((lg=send(client_socket,"Taille du fichier reçu !!!",512,0))==-1){
+						fprintf(stderr, "Failure Sending Confirmation size file\n");
+						close(client_socket);
+						exit(1);
+					}
+					unsigned char cipher_content[size_file];
+					
+					if((lg=recv(client_socket,cipher_content,size_file,0))==-1){
+						fprintf(stderr, "Failure Receving Cipher text\n");
+						close(client_socket);
+						exit(1);
+					}
+					//strcpy(content_file,buffer);
+					//printf("%s\n",content_file);
+					//hexdump(stdout, "AES_ENCRYPT", (unsigned char*) &cipher_content, sizeof(cipher_content));
+					if((lg=send(client_socket,"Contenu du fichier reçu !!!",512,0))==-1){
+						fprintf(stderr, "Failure Sending Confirmation cipher text\n");
+						close(client_socket);
+						exit(1);
+					}
+					unsigned char dec_out[size_file];
+					unsigned char iv[AES_BLOCK_SIZE];
+					memset(iv,0,AES_BLOCK_SIZE);
+					AES_KEY dec_key;
+					
+					AES_set_decrypt_key((unsigned char*) key,128, &dec_key);
+					AES_cbc_encrypt(cipher_content, dec_out, size_file, &dec_key, iv, AES_DECRYPT);
+
+					char *chaine_dechiffree = (char *) dec_out;
+					//printf("%s\n",chaine_dechiffree);
+				}
+			}
 			shutdown(client_socket,2);
 			close(client_socket);
 			exit(0);
 		}
-                memset(file_name,0,500);
+                /*memset(file_name,0,500);
 		memset(content_file,0,size_file);
-		size_file=0;
+		size_file=0;*/
 	}
 	shutdown(ma_socket,2);
 	close(ma_socket);
